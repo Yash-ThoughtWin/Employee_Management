03da87a9d24c7bbb952758470578b705b8902fcf
Yash-ThoughtWin <yashonil@thoughtwin.com>
2026-06-08 12:50:36 +0530

    First Code Push

---
+++ app/main.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI
+#from app.schemas import Employee
+
+app = FastAPI()
+
+employees = [
+    {"id": 1, "name": "Yash", "Role":"Pyhton"},
+    {"id": 2, "name": "Abhishek", "Role":"Pyhton"},
+    {"id": 3, "name": "Lavesh", "Role":"Pyhton"},
+    {"id": 4, "name": "Mayank", "Role":"Pyhton"}
+]
+
+tasks = [
+    {"id": 1, "task": "FastAPI"},
+    {"id": 2, "task": "GIT"},
+    {"id": 3, "task": "GenAI"}
+]
+
+@app.get("/")
+def home(): 
+    return {"message": "Welcome to Git"}
+
